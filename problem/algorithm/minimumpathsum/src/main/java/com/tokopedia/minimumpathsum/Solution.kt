ae6e5f764f562493462bd772ce08704b39705b1d
Nurholis <holis.nur072219@gmail.com>
2021-05-05 17:24:27 +0700

    menamabahka sebuah solusi dari algoritma minimum path sum

--- problem/algorithm/minimumpathsum/src/main/java/com/tokopedia/minimumpathsum/Solution.kt
+++ problem/algorithm/minimumpathsum/src/main/java/com/tokopedia/minimumpathsum/Solution.kt
@@ -4,26 +4,29 @@ object Solution {
     fun minimumPathSum(matrix: Array<IntArray>): Int {
         // TODO, find a path from top left to bottom right which minimizes the sum of all numbers along its path, and return the sum
         // below is stub
-       val rows = matrix.size
-       val cols = matrix[0].size
-       val arrayMatrix = arrayOf(
-               intArrayOf(rows),
-               intArrayOf(cols)
-       )
-       for (i in matrix.indices) {
-           for (j in arrayMatrix.indices) {
-               if (i == rows-1 && cols != cols-1 ) {
-                   arrayMatrix[i][j] = matrix[i][j] + arrayMatrix[i][j+1]
-               } else if (j == cols-1 && i != rows-1 ) {
-                   arrayMatrix[i][j] = matrix[i][j] + arrayMatrix[i+1][j]
-               } else if (i != rows-1 && j != cols-1) {
-                   arrayMatrix[i][j] = matrix[i][j]  + arrayMatrix[i + 1][j].coerceAtLeast(arrayMatrix[i][j + 1])
-               } else {
-                   arrayMatrix[i][j] = matrix[i][j]
-               }
-           }
-       }
-       return arrayMatrix[0][0]
+        if (matrix.isEmpty()) {
+            return 0
+        }
+      return sumPath(0,0, matrix)
     }
 
+    private fun sumPath(
+        si: Int,
+        sj: Int,
+        matrix: Array<IntArray>
+    ) : Int {
+        val ei = matrix.size-1 //get last index
+        val ej = matrix[0].size-1
+
+        if (si == ei && sj == ej)
+            return matrix[si][sj]
+
+        if (si > ei || sj > ej)
+            return Int.MAX_VALUE
+
+        val op1 = sumPath(si+1, sj, matrix)
+        val op2 = sumPath(si, sj+1, matrix)
+
+        return matrix[si][sj] + Math.min(op1, op2)
+    }
 }
