c8e3beb40ff21deade989e2fbb9ca7777addc5f0
Nurholis <holis.nur072219@gmail.com>
2021-05-06 15:22:40 +0700

    menambahkan pengaturan retrofit dan data class

---
+++ problem/android/filter/src/main/java/com/tokopedia/filter/view/view/FilterActivity.kt
@@ -0,0 +1,12 @@
+package com.tokopedia.filter.view.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tokopedia.filter.R
+
+class FilterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_filter)
+    }
+}
